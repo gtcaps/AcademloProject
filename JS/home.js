568c3f936782859bfb9148e4841edd506315078f
Camilo <c.acevedo1993@gmail.com>
2019-08-20 14:56:44 -0500

    Added function to load the 3 first products

--- JS/home.js
+++ JS/home.js
@@ -1,6 +1,46 @@
 const urlApi = "http://fundamentos.academlo.com/api/v1";
 const category_id = "5c82982e-b63e-4280-8287-4eba0e99716a"; //Prueba Libros - Reemplazar por el de programming
 
+/**
+ La variable "n" que recibe la funcion es el indice en el arreglo de categories. 
+ */
+
+async function getCategoriesProducts(n){
+    /** API CALLS */
+    let apiData = await fetch(`${urlApi}/directories/${category_id}/categories`);
+    let categoriesData = await apiData.json();
+    
+    let products = await fetch(`${urlApi}/categories/${categoriesData.categories[n].uuid}/products`);
+    let productsData = await products.json();
+
+    let arrayP = productsData.products;
+    let colData = '';
+
+    for( let i = 0; i < 3; i++ ) {
+        let rowData = document.getElementById('row-data');
+        colData += `<div class="col-md-4 col-card card-style col-12">
+            <div class="card">
+                <div class="card-img">
+                    <img src="${arrayP[i].image}" alt="..." />
+                </div>
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <div class="p-3">
+                        <h2 class="card-title">${arrayP[i].name}</h2>
+                        <p class="card-text">
+                            ${arrayP[i].description}
+                        </p>
+                    </div>
+                    <div>
+                        <a href="${arrayP[i].url}" target=_blank class="btn btn-transparent btn-block">Visite site</a>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+        rowData.innerHTML = colData;
+    }
+}
+
 async function getCategories(){
     let apiData = await fetch(`${urlApi}/directories/${category_id}/categories`);
     let categoriesData = await apiData.json();
@@ -31,3 +71,4 @@ async function getCategories(){
 }
 
 getCategories();
+getCategoriesProducts(5);
